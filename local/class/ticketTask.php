87db52bf09f5fc5a406f7f0bd847497723ff77fe
alexey <av@inalytics.ru>
2019-02-11 21:20:01 +0300

    toLead::addLead()

--- local/class/ticketTask.php
+++ local/class/ticketTask.php
@@ -32,5 +32,49 @@ class toLead {
             return 'N';
         }
     }
+    private function addLead($name, $lastName, $group){
+        global $DB;
+        if(
+            (self::getContact($name, $lastName) == 'N' ||
+                self::getLead($name, $lastName) == 'N') and
+            $group == 7
+        ){
+            $DB->Query("
+                insert into b_crm_lead
+                (
+                  DATE_CREATE, 
+                  DATE_MODIFY, 
+                  CREATED_BY_ID, 
+                  MODIFY_BY_ID, 
+                  ASSIGNED_BY_ID, 
+                  OPENED, 
+                  STATUS_ID, 
+                  SOURCE_DESCRIPTION, 
+                  TITLE,
+                  FULL_NAME, 
+                  NAME, 
+                  LAST_NAME, 
+                  COMMENTS
+                ) value (
+                  'NOW()', 
+                  'NOW()', 
+                  '1', 
+                  '1', 
+                  '1', 
+                  'Y', 
+                  'NEW', 
+                  'Форма регистрации ЛК ТП',
+                  '".$lastName." ".$name.": Заяка на регистрацию ЛК ТП', 
+                  '".$lastName." ".$name."', 
+                  '".$name."', 
+                  '".$lastName."', 
+                  'Подана заявка на регистрацию личного кабинета Технической Поддержки от: ".$lastName." ".$name."'
+                )
+            ");
+            return;
+        } else {
+            return;
+        }
+    }
 }
 ?>
