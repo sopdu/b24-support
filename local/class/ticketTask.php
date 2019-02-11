d56269534bbcaac31104a2931fc6ef332927f825
alexey <av@inalytics.ru>
2019-02-11 21:38:12 +0300

    toLead::addTask()

--- local/class/ticketTask.php
+++ local/class/ticketTask.php
@@ -83,5 +83,25 @@ class toLead {
         ");
         return $zapros->Fetch();
     }
+    private function addTask($name, $lastName, $group){
+        if($group == 7){
+            CModule::IncludeModule("tasks");
+            $obTask = new CTasks;
+            $obTask->Add(
+                array(
+                    "TITLE"                 =>  'В тех поддержке зарегистрировался новый пользователь',
+                    "DESCRIPTION"           =>  'Зарегистрировался новый поьзователь в тех поддержке: <strong>'.$name.' '.$lastName.'</strong>. На основании его создал лид',
+                    "AUDITORS"              =>  array(8),
+                    "ACCOMPLICES"           =>  array(8),
+                    "ALLOW_TIME_TRACKING"   =>  'Y',
+                    "TAGS"                  =>  'Регистрация в тех поддержке',
+                    "ALLOW_CHANGE_DEADLINE" =>  'Y',
+                    "TASK_CONTROL"          =>  'Y',
+                    "RESPONSIBLE_ID"        =>  9
+                )
+            );
+        }
+        return;
+    }
 }
 ?>
