d7075abd81f467d98f44f641e2e7336dff8113b5
alexey <av@inalytics.ru>
2019-02-14 13:39:04 +0300

    addNotify()

--- local/class/ticketTask.php
+++ local/class/ticketTask.php
@@ -112,5 +112,22 @@ class toLead {
         }
         return;
     }
+    private function addNotify(){
+        CAdminNotify::Add(
+            array(
+                "MESSAGE"   =>  'Зарегистрировался новый пользователь технической поддержки: '.self::getMaxIdLead()["FULL_NAME"].'. <a target="_blank" href="http://194.87.244.74/crm/lead/details/'.self::getMaxIdLead()["ID"].'">Перейти к списку</a>'
+            )
+        );
+        CIMNotify::Add(
+            array(
+                "FROM_USER_ID" => 1,
+                "TO_USER_ID" => 7,
+                "NOTIFY_TYPE" => IM_NOTIFY_SYSTEM,
+                "NOTIFY_MODULE" => "im",
+                "NOTIFY_TAG"    => 'support',
+                "NOTIFY_MESSAGE" => 'Зарегистрировался новый пользователь технической поддержки: '.self::getMaxIdLead()["FULL_NAME"].'. <a target="_blank" href="http://194.87.244.74/crm/lead/details/'.self::getMaxIdLead()["ID"].'/">Перейти</a>'
+            ));
+        return;
+    }
 }
 ?>
